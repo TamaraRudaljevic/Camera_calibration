74f740452cdeb7b5ec6e2363cf0cc3c5c31f9063
TamaraRudaljevic <tamararudaljevicruda@gmail.com>
2020-05-19 09:53:41 +0200

    Begining

--- calibration/calibration.py
+++ calibration/calibration.py
@@ -17,6 +17,15 @@ def corner(img):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 390, 410)
     lines = cv2.HoughLines(edges, 1, np.pi/180, 55)
+
+    h = []
+    v = []
+
+    for rho, theta in lines[0]:
+        if(abs(np.tan(theta)) > 1):
+            v.append([rho, theta])
+        else:
+            h.append([rho, theta])
     
     for line in lines:
         rho, theta = line[0]
@@ -31,12 +40,12 @@ def corner(img):
 
 
 # creating marix A
-# running svd(A) => H
+# svd(A) => H
 def Homography(p, world):
     a = []
     point = np.asarray(p)
 
-    # calculating matrix A
+    # calculating matrix A from points and world points
     for p in range(len(point)):
         p1 = world[p]
         p2 = np.append(point[p], 1)
@@ -69,19 +78,40 @@ def Intrinsic(point, world):
         list_v.append(v2)
         
     V = np.matrix(list_v)
-
     u, s, v = np.linalg.svd(V)
-
     b = v[5]
 
-    v0 = (b.item(1)*b.item(2) - (b.item(0)*b.item(5)))/ (b.item(0) * b.item(3) - b.item(1)**2)
-    lam = b.item(8) - (b.item(2)**2 + v0*(b.item(1)*b.item(2) - b.item(0)*b.item(5))) / b.item(0)
+    v0 = (b.item(1)*b.item(3) - b.item(0)*b.item(4))/ (b.item(0) * b.item(2) - b.item(1)**2)
+    lam = b.item(5) - (b.item(3)**2 + v0*(b.item(1)*b.item(3) - b.item(0)*b.item(4))) / b.item(0)
     alpha = np.sqrt(lam/b.item(0))
-    beta = np.sqrt(lam*b.item(0) / (b.item(0)*b.item(4) - b.item(1)**2))
+    beta = np.sqrt(lam*b.item(0) / (b.item(0)*b.item(2) - b.item(1)**2))
     gama = -b.item(1)*alpha**2*beta/lam
-    u0 = gama*v0/beta - b.item(2)*alpha**2/gama
+    u0 = gama*v0/beta - b.item(3)*alpha**2/gama
 
     k = np.array([[alpha, gama, u0], [0, beta, v0], [0, 0, 1]])
     return k
+
+def Extrinsic(intrinsic, cornerPoint, worldPoint):
+    h = Homography(cornerPoint, worldPoint)
+    h = np.linalg.inv(h)
+    intrinsicsInv = np.linalg.inv(intrinsic)
+
+    h1 = h[:, 0]
+    h2 = h[:, 1]
+    h3 = h[:, 2]
+
+    lam_r1 = 1 / np.linalg.norm(np.dot(intrinsicsInv, h1))
+    lam_r2 = 1 / np.linalg.norm(np.dot(intrinsicsInv, h2))
+    lam_r3 = (lam_r1 + lam_r2) / 2
+    r1 = lam_r1 * np.dot(intrinsicsInv, h1)
+    r2 = lam_r2 * np.dot(intrinsicsInv, h2)
+    r3 = np.cross(r1, r2, axis=0)
+    t = np.array(lam_r3 * np.dot(intrinsicsInv, h3)).transpose()
+
+    r = np.append(r1, r2, axis=1)
+    r = np.append(r, r3, axis=1)
+    rt = np.append(r, np.transpose(t), axis=1)
+    return rt
+    
 
 image = "/home/tamarar/Desktop/Camera_calibration/calibration/calibration_test.png"
